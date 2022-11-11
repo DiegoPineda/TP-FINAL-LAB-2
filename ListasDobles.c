1e125d857831e358262101478578d31f862faff9
DiegoPineda <diegopineda819@gmail.com>
2022-11-11 19:57:36 -0300

    cambios listas dobles

--- ListasDobles.c
+++ ListasDobles.c
@@ -3,48 +3,6 @@
 #include "string.h"
 #include "windows.h"
 
-///Estructuras para el manejo de listas dobles:
-
-typedef struct
-{
-    usuario dato;
-    struct nodo2* anterior;
-    struct nodo2* siguiente;
-} nodo2;
-
-typedef struct
-{
-    struct nodo2* adelante;
-    struct nodo2* atras;
-} fila;
-
-
-///Funciones multi-uso:
-nodo2* inicLista();
-nodo2* crearNodo(jugador dato);
-nodo2* agregarPrincipio(nodo2* lista,nodo2* nuevoNodo);
-nodo2* buscarUltimo(nodo2* lista);
-nodo2* agregarFinal(nodo2* lista,nodo2* nuevoNodo);
-nodo2* agregarEnOrden(nodo2* lista,nodo2* nuevoNodo);
-char preguntar();
-void mostrarLista(nodo2* lista);
-///Funciones para el ejercicio 1:
-nodo2* cargarLista(nodo2* lista);
-nodo2* borrarNodo(char nombre[20],nodo2* lista);
-
-///Funciones para el ejercicio 3:
-void inicFila(fila* cola);
-void mostrarFila(fila* cola);
-void agregaFinal(fila* cola, nodo2* nuevoNodo);
-void leerFila(fila* cola);
-int filavacia(fila* cola);
-
-int main()
-{
-    menu();
-    return 0;
-}
-
 ///Funciones multi-uso:
 char preguntar()
 {
